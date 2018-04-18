9d81d8caa65fc6c107a5113d1bfaed327625cd62
jam1garner <jam1.mcleod@hotmail.com>
2018-04-18 10:14:00 -0400

    Fix strings being switched from shorts to ints when shorts are disabled

--- msclang.py
+++ msclang.py
@@ -331,7 +331,7 @@ def compileNode(node, loopParent=None, parentLoopCondition=None):
         elif node.type == "string":
             if not node.value[1:-1] in msc.strings:
                 msc.strings.append(node.value[1:-1])
-            nodeOut.append(Command(0xD if args.usePushShort else 0xA, [msc.strings.index(node.value[1:-1])]))
+            nodeOut.append(Command(0xD, [msc.strings.index(node.value[1:-1])]))
     elif t == c_ast.Assignment:
         nodeOut += compileNode(node.rvalue, loopParent, parentLoopCondition)
         addArg()
